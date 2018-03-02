f17e6cc40cc9a19276514b39d2132e0a3df25813
Eric Albertus <ERAlchemist@gmail.com>
2018-03-01 20:42:53 -0800

    Edit Requests feature added

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -16,6 +16,16 @@ class RequestsController < ApplicationController
         redirect_to request_path(@request)
     end
 
+    def edit
+        @request = Request.find(params[:id])
+    end
+
+    def update
+        @request = Request.find(params[:id])
+        @request.update request_params
+        redirect_to request_path(@request)
+    end
+
     private
         def request_params
             params.require(:request).permit(:title, :description, :due_date)
